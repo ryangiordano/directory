5c32dc61adabf9b342940a2bf6e9f1539f5226bd
Ryan Giordano <rgiorda1@gmail.com>
2016-11-08 23:45:46 -0500

    updated angular cli

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('App: Directory2', () => {
+describe('App: CodigoDirectory', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
---
+++ src/server/app.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var path = require('path');
+// var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var engine = require('consolidate');
+
+var appRoutes = require('./routes/appRoutes');
+var userRoutes = require('./routes/users');
+var employeeRoutes = require('./routes/employees');
+
+var app = express();
+///---------SET UP SEED DATA
+if (app.get('env') === 'development'){
+
+  var setupController = require('./controllers/setupController');
+  setupController(app);
+
+}
+////------///
+
+
+
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(__dirname + '/../../dist'));
+app.use('/', express.static(__dirname + '/../public'));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
+
+app.set('views', path.join(__dirname, 'src'));
+app.engine('html', engine.handlebars);
+app.set('view engine', 'html');
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE');
+  next();
+});
+
+app.use(logger('dev'));
+
+//mongoose stuff
+mongoose.connect('thedaruma:test123@ds145677.mlab.com:45677/directory');
+
+var db = mongoose.connection;
+mongoose.Promise = global.Promise;
+
+//routes
+app.use('/', appRoutes);
+app.use('/users-api', userRoutes);
+app.use('/employees-api', employeeRoutes);
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+
+// all other routes are handled by Angular
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname,'/../../dist/index.html'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Codigo Directory listening on port ' + app.get('port'));
+});
